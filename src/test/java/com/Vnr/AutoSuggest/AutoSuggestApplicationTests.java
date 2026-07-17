ca6ece76cbea787dccc2922d105c07f0966a2d93
Sravasthi Goli <sravasthigoli07@gmail.com>
2026-07-17 14:45:31 +0530

    initial

---
+++ src/test/java/com/Vnr/AutoSuggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vnr.AutoSuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
