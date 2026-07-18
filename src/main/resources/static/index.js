7d5235753cfac85b359979dfc4d665b6301b9670
Sravasthi Goli <sravasthigoli07@gmail.com>
2026-07-18 12:13:11 +0530

    toggle user implemented

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,37 @@
 // alert("hello" ,+name);
 console.log("Hello from js");
+
+//data = {
+
+//    "name" : "Jane",
+//    "gender" : "Female"
+//}
+
+//data.name // Jane
+
+
+var users = [
+    {
+        "name" : "Sravasthi",
+        "gender" : "Female",
+        "image" : "jane.png"
+    },
+    {
+        "name" : "John doe",
+        "gender" : "Male",
+        "image" : "john.png"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].image;
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+    var gender = document.getElementById("user-gender");
+    usergender.innerHTML = users[id].gender;
+
+}
